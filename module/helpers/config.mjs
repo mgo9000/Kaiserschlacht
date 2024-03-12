eb5b7e0ba8248c42a3eaee3e95bfa38007f7a58d
mgo9000 <mgo9000@gmail.com>
2024-03-11 23:20:56 -0400

    Hopefully fixing the sheet for real

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -7,17 +7,43 @@ export const KAISERSCHLACHT = {};
 KAISERSCHLACHT.abilities = {
   physique: 'KAISERSCHLACHT.Ability.Physique.long',
   finesse: 'KAISERSCHLACHT.Ability.Finesse.long',
-  con: 'KAISERSCHLACHT.Ability.Con.long',
-  int: 'KAISERSCHLACHT.Ability.Int.long',
   wits: 'KAISERSCHLACHT.Ability.Wits.long',
   presence: 'KAISERSCHLACHT.Ability.Presence.long',
+};
+KAISERSCHLACHT.skills = {
+  brawl: 'KAISERSCHLACHT.Skill.Brawl.long',
+  athletics: 'KAISERSCHLACHT.Skill.Athletics.long',
+  smash: 'KAISERSCHLACHT.Skill.Smash.long',
+  shooting: 'KAISERSCHLACHT.Skill.Shooting.long',
+  skullduggery: 'KAISERSCHLACHT.Skill.Skullduggery.long',
+  acrobatics: 'KAISERSCHLACHT.Skill.Acrobatics.long',
+  knowledgeMen: 'KAISERSCHLACHT.Skill.KnowledgeMen.long',
+  knowledgeMachines: 'KAISERSCHLACHT.Skill.KnowledgeMachines.long',
+  knowledgeMagic: 'KAISERSCHLACHT.Skill.KnowledgeMagic.long',
+  empathy: 'KAISERSCHLACHT.Skill.Empathy.long',
+  manipulation: 'KAISERSCHLACHT.Skill.Manipulation.long',
+  intimidation: 'KAISERSCHLACHT.Skill.Intimidation.long',
+
 };
 
 KAISERSCHLACHT.abilityAbbreviations = {
   physique: 'KAISERSCHLACHT.Ability.Physique.abbr',
   finesse: 'KAISERSCHLACHT.Ability.Finesse.abbr',
-  con: 'KAISERSCHLACHT.Ability.Con.abbr',
-  int: 'KAISERSCHLACHT.Ability.Int.abbr',
   wits: 'KAISERSCHLACHT.Ability.Wits.abbr',
   presence: 'KAISERSCHLACHT.Ability.Presence.abbr',
+};
+KAISERSCHLACHT.skillAbbreviations = {
+  brawl: 'KAISERSCHLACHT.Skill.Brawl.abbr',
+  athletics: 'KAISERSCHLACHT.Skill.Athletics.abbr',
+  smash: 'KAISERSCHLACHT.Skill.Smash.abbr',
+  shooting: 'KAISERSCHLACHT.Skill.Shooting.abbr',
+  skullduggery: 'KAISERSCHLACHT.Skill.Skullduggery.abbr',
+  acrobatics: 'KAISERSCHLACHT.Skill.Acrobatics.abbr',
+  knowledgeMen: 'KAISERSCHLACHT.Skill.KnowledgeMen.abbr',
+  knowledgeMachines: 'KAISERSCHLACHT.Skill.KnowledgeMachines.abbr',
+  knowledgeMagic: 'KAISERSCHLACHT.Skill.KnowledgeMagic.abbr',
+  empathy: 'KAISERSCHLACHT.Skill.Empathy.abbr',
+  manipulation: 'KAISERSCHLACHT.Skill.Manipulation.abbr',
+  intimidation: 'KAISERSCHLACHT.Skill.Intimidation.abbr',
+
 };
