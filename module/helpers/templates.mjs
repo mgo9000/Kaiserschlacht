f430ec26595fb75bd2d54a989af442e1b7a3d247
mgo9000 <mgo9000@gmail.com>
2024-03-11 21:13:26 -0400

    Add files via upload

---
+++ module/helpers/templates.mjs
@@ -0,0 +1,16 @@
+/**
+ * Define a set of template paths to pre-load
+ * Pre-loaded templates are compiled and cached for fast access when rendering
+ * @return {Promise}
+ */
+export const preloadHandlebarsTemplates = async function () {
+  return loadTemplates([
+    // Actor partials.
+    'systems/kaiserschlacht/templates/actor/parts/actor-features.hbs',
+    'systems/kaiserschlacht/templates/actor/parts/actor-items.hbs',
+    'systems/kaiserschlacht/templates/actor/parts/actor-spells.hbs',
+    'systems/kaiserschlacht/templates/actor/parts/actor-effects.hbs',
+    // Item partials
+    'systems/kaiserschlacht/templates/item/parts/item-effects.hbs',
+  ]);
+};
