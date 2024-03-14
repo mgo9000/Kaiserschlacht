f60cd9b99da74da6b490465293d67e04ab4fc141
mgo9000 <mgo9000@gmail.com>
2024-03-14 18:44:38 -0400

    333

--- module/helpers/dice-dialogue.js
+++ module/helpers/dice-dialogue.js
@@ -6,34 +6,35 @@ const diffMod = await Dialog.wait({
     buttons: {
         one: { icon: '<i class="roll die d4"></i>',
         label: "d4", callback: () => {
-            const diffMod = "- 1d4"
+            const diffMod = " - 1d4"
             return diffMod;
       }},
       two: { icon: '<i class="roll die d6"></i>',
         label: "d6", callback: () => {
-            const diffMod = "- 1d6"
+            const diffMod = " - 1d6"
             return diffMod;
       }},
       three: { icon: '<i class="roll die d8"></i>',
         label: "d8", callback: () => {
-            const diffMod = "- 1d8"
+            const diffMod = " - 1d8"
             return diffMod;
       }},
       four: { icon: '<i class="roll die d6"></i>',
         label: "d10", callback: () => {
-            const diffMod = "- 1d10"
+            const diffMod = " - 1d10"
             return diffMod;
       }},
       five: { icon: '<i class="roll die d6"></i>',
         label: "d12", callback: () => {
-            const diffMod = "- 1d12"
+            const diffMod = " - 1d12"
             return diffMod;
       }},
       
     },
   });
+    let amendedFormula = (formula + diffmod);
     let label = labelfromSheet;
-      let roll = new Roll(formula.concat(diffMod), actorObject.getRollData());
+      let roll = new Roll(amendedFormula, actorObject.getRollData());
       roll.toMessage({
         speaker: ChatMessage.getSpeaker({ actor: actorObject }),
         flavor: label,
