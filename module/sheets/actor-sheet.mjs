90bb8bdc5f0fe107981dc09f928bf7154b706181
mgo9000 <mgo9000@gmail.com>
2024-03-15 02:37:57 -0400

    typo fixed

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -244,7 +244,7 @@ export class KaiserschlachtActorSheet extends ActorSheet {
         if (item) return item.roll();
       }
       else if (dataset.rollType == 'reload') {
-        const itemId = element.closest('.weapon').dataset.itemId;
+        const itemId = element.closest('.item').dataset.itemId;
         const item = this.actor.items.get(itemId);
         if (item) return item.roll();
       }
