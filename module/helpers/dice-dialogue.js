a747abcbfdbda2be703023c280a59fb1f071246a
mgo9000 <mgo9000@gmail.com>
2024-03-19 19:36:15 -0400

    Fixed difficulty dice on NPC sheets for realsies

--- module/helpers/dice-dialogue.js
+++ module/helpers/dice-dialogue.js
@@ -6,27 +6,27 @@ const diffMod = await Dialog.wait({
     buttons: {
         one: { icon: '<class="roll die d4">',
         label: "d4", callback: () => {
-            const diffMod = " - 1d4[difficulty]"
+            const diffMod = " - 1d4[diff.]"
             return diffMod;
       }},
       two: { icon: '<class="roll die d6">',
         label: "d6", callback: () => {
-            const diffMod = " - 1d6[difficulty]"
+            const diffMod = " - 1d6[diff.]"
             return diffMod;
       }},
       three: { icon: '<class="roll die d8">',
         label: "d8", callback: () => {
-            const diffMod = " - 1d8[difficulty]"
+            const diffMod = " - 1d8[diff.]"
             return diffMod;
       }},
       four: { icon: '<class="roll die d10">',
         label: "d10", callback: () => {
-            const diffMod = " - 1d10[difficulty]"
+            const diffMod = " - 1d10[diff.]"
             return diffMod;
       }},
       five: { icon: '<class="roll die d12">',
         label: "d12", callback: () => {
-            const diffMod = " - 1d12[difficulty]"
+            const diffMod = " - 1d12[diff.]"
             return diffMod;
       }},
       
