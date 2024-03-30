98335521b989a468922cb5206560bedd3ebdca26
mgo9000 <mgo9000@gmail.com>
2024-03-29 20:21:15 -0400

    target token should now be grabbed upon making a roll

--- module/helpers/roll.mjs
+++ module/helpers/roll.mjs
@@ -5,18 +5,21 @@
  * @param {object} [options.targetedToken]   The token which is provided as a target.
  * @param {boolean} [options.diffRoll]   Whether or not it is a roll against a difficulty die.
  */
-export class KSRoll extends Roll{
-/** @override */
-static CHAT_TEMPLATE = "systems/kaiserschlacht/templates/chat/roll.hbs";
+export class KSRoll extends Roll {
+   /** @override */
+   static CHAT_TEMPLATE = "systems/kaiserschlacht/templates/chat/roll.hbs";
+   /* -------------------------------------------- */
 
- /** @inheritdoc */
- async toMessage(messageData={}, options={}) {
-   if ( !this._evaluated ) await this.evaluate({async: true});
-   messageData.rolls = [this];
-   messageData.rolls.targetNumber = game.user.targets.first()?.document.actor.system.targetNumber ?? null;
-   console.log(game.user.targets.first() ?? null);
-   console.log(messageData.rolls.targetNumber);
-    return super.toMessage(messageData, options);
- }
+   /**
+    * Return the target number from the data of a targetted token.
+    * @type {string}
+    */
+   get targetNumber() {
+      return messageData.rolls.targetNumber = game.user.targets.first()?.document.actor.system.targetNumber ?? null;
+   }
+   /** @inheritdoc */
+   async toMessage(messageData = {}, options = {}) {
+      return super.toMessage(messageData, options);
+   }
 }
 
