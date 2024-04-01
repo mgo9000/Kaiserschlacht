f224a4b36c436b7710950bb118a14aa26f82af23
mgo9000 <mgo9000@gmail.com>
2024-04-01 19:53:14 -0400

    fixed context in damage button

--- module/documents/chat-message.mjs
+++ module/documents/chat-message.mjs
@@ -22,7 +22,7 @@ export class KSChatMessage extends ChatMessage {
     const a = event.currentTarget
     let dataset = a.dataset;
     const targetTokens = canvas.tokens.controlled;
-    targetTokens.forEach(token => token.actor._applyDamage(dataset.damage, dataset.damagetags));
+    targetTokens.forEach(token => token.actor._applyDamage(dataset.damage, dataset.damagetags), token.actor);
 
   }
   _onClickChatReload(event) {
