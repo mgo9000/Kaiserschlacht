a889190dccbe69c62d988ec69a96231ed296d788
mgo9000 <mgo9000@gmail.com>
2024-04-12 00:49:25 -0400

    changed stat manager base class

--- module/helpers/stat-manager.mjs
+++ module/helpers/stat-manager.mjs
@@ -6,18 +6,28 @@
  * @param {string} [options.statType=null]
  * @param {string} [options.stat=null]  
  */
-export class StatManager extends FormApplication {
+export class StatManager extends DocumentSheet {
     constructor(actor, options = {}) {
         super(options);
 
-        /**
-         * The original actor
-         * @type {KSActor}
-         */
         this.actor = actor;
         this.statType = options.statType;
         this.stat = options.stat;
 
+    }
+    /** @inheritdoc */
+    getData() {
+        const data = this.document.toObject(false);
+        return {
+            cssClass: "editable",
+            editable: true,
+            statType: this.statType,
+            stat: this.stat,
+            document: this.actor,
+            data: data,
+            options: this.options,
+            title: this.title
+        };
     }
 
 
