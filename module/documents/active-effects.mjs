7dfe5f62af9b7fad5ed34aa282b7d013d710f3c4
mgo9000 <mgo9000@gmail.com>
2024-04-16 18:57:03 -0400

    changing duration updation function

--- module/documents/active-effects.mjs
+++ module/documents/active-effects.mjs
@@ -67,6 +67,7 @@ export class KSActiveEffect extends ActiveEffect {
       new this(effectData, options)
     );
   }
+
   /**
    * Manages the duration of a given effect, signaling to remove it if expired.
    */
@@ -74,5 +75,21 @@ export class KSActiveEffect extends ActiveEffect {
     const effect = document;
     if (document.duration.remaining <= 0) {
     }
+  }
+
+  /**
+   * Update derived Active Effect duration data.
+   * Configure the remaining and label properties to be getters which lazily recompute only when necessary.
+   * @override
+   * @returns {ActiveEffectDuration}
+   */
+  updateDuration() {
+    console.log(this.duration);
+    const { remaining, label, ...durationData } = this._prepareDuration();
+    console.log(remaining);
+    console.log(label);
+    console.log(durationData);
+    Hooks.callAll("updateDuration");
+    return super.updateDuration();
   }
 }
