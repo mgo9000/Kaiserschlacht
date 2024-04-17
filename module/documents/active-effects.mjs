72770fb01f4515bfddb9be97a830ba32445e7fea
mgo9000 <mgo9000@gmail.com>
2024-04-17 16:48:22 -0400

    updated prone application

--- module/documents/active-effects.mjs
+++ module/documents/active-effects.mjs
@@ -10,8 +10,22 @@ export default class KSActiveEffect extends ActiveEffect {
     console.log(arguments);
     console.log(this);
     if (this.modifiesActor) {
-      if (this.onRemove) {
-        this.target[onRemove];
+      if (this.flags.onRemove) {
+        actor = this.target;
+        const onRemoveFunc = onRemove;
+        const onRemoveArgs = this.flags.onRemoveArgs;
+        switch (onRemoveFunc) {
+          case toggleTokenStatus:
+            const actorTokens = actor.getActiveTokens(true, true);
+            actorTokens.forEach((token) =>
+              token.toggleActiveEffect(
+                CONFIG.statusEffects.find((e) => e.onRemoveArgs.id)
+              )
+            );
+            break;
+          default:
+            break;
+        }
       }
     }
     super._onDelete(options, userId);
