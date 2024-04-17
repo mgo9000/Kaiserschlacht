f1157e07598114bd0f18e3c46e48187c5723b810
mgo9000 <mgo9000@gmail.com>
2024-04-17 19:16:53 -0400

    logging improvements

--- module/documents/active-effects.mjs
+++ module/documents/active-effects.mjs
@@ -11,6 +11,7 @@ export default class KSActiveEffect extends ActiveEffect {
     await super._preCreate(data, options, user);
     console.log(this);
     if (this.flags.startOfNext) {
+      console.log("applying start of next turn duration adjustment");
       const cbt = game.combat;
       const d = this.duration;
       if (cbt) {
@@ -19,9 +20,11 @@ export default class KSActiveEffect extends ActiveEffect {
           turn: cbt.turn ?? 0,
           nTurns: cbt.turns.length || 1,
         };
-        let newDTurns = c.nTurns - d.startTurn;
+        console.log(c);
+        const newDTurns = c.nTurns - d.startTurn;
         const current = this._getCombatTime(c.round, c.turn);
         const duration = this._getCombatTime(0, newDTurns);
+        console.log(duration);
         const start = this._getCombatTime(d.startRound, d.startTurn, c.nTurns);
         const durationLabel = this._getDurationLabel(0, newDTurns);
         const remaining = Math.max(
